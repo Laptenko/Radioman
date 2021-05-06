bded9e450b7dbd90911678324c307d389795eee2
Maksim Laptenko <laptenko0811@icloud.com>
2021-05-06 17:33:32 +0300

    update Radio and RadioTest

--- src/main/java/ru/netology/damain/Radio.java
+++ src/main/java/ru/netology/damain/Radio.java
@@ -1,10 +1,75 @@
 package ru.netology.damain;
 
 public class Radio {
-    int station;
-    int maxStation;
-    int minStation;
+    private int station;
+    private int maxStation = 9;
+    private int minStation = 0;
 
+    public int getStation() {
+        return station;
+    }
 
+    public void setStation(int station) {
+      if (station > maxStation){
+          return;
+      }
+      if (station < minStation){
+          return;
+      }
+      this.station = station;
+    }
 
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+    public int getMinStation() {
+        return minStation;
+    }
+
+    public void next(int station) {
+        if (station >= 9) {
+            this.station = 0;
+            return;
+        }
+        this.station = station + 1;
+    }
+
+    public void prev(int station) {
+        if (station <= 0) {
+            this.station = 9;
+            return;
+        }
+        this.station = station - 1;
+    }
+
+    private int volume;
+    private int maxVolume = 10;
+    private int minVolume = 0;
+
+    public int getVolume() {
+        return volume;
+    }
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void plusVolume(int volume){
+        if (volume >= maxVolume){
+            this.volume = maxVolume;
+            return;
+        }
+        this.volume = volume + 1;
+    }
+
+    public void minusVolume(int volume){
+        if (volume <= minVolume){
+            this.volume = minVolume;
+            return;
+        }
+        this.volume = volume - 1;
+    }
 }
--- src/test/java/ru/netology/damain/RadioTest.java
+++ src/test/java/ru/netology/damain/RadioTest.java
@@ -5,12 +5,95 @@ import static org.junit.jupiter.api.Assertions.*;
 public class RadioTest {
 
     @Test
-    public void createRadio(){
+    public void maxStation(){
         Radio radio = new Radio();
-        radio.maxStation = 9;
-        radio.minStation = 0;
-        assertEquals(9, radio.maxStation);
-        assertEquals(0, radio.minStation);
+        assertEquals(9, radio.getMaxStation());
     }
-
+    @Test
+    public void minStation(){
+        Radio radio = new Radio();
+        assertEquals(0, radio.getMinStation());
+    }
+    @Test
+    public void updateNextStation(){
+        Radio radio = new Radio();
+        radio.next(5);
+        assertEquals(6, radio.getStation());
+    }
+    @Test
+    public void updateLimitMaxStation(){
+        Radio radio = new Radio();
+        radio.next(9);
+        assertEquals(0, radio.getStation());
+    }
+    @Test
+    public void updateOverMaxStation(){
+        Radio radio = new Radio();
+        radio.next(523);
+        assertEquals(0, radio.getStation());
+    }
+    @Test
+    public void updatePrevStation(){
+        Radio radio = new Radio();
+        radio.prev(6);
+        assertEquals(5, radio.getStation());
+    }
+    @Test
+    public void updateUnderLimitPrevStation(){
+        Radio radio = new Radio();
+        radio.prev(0);
+        assertEquals(9, radio.getStation());
+    }
+    @Test
+    public void updateStation(){
+        Radio radio = new Radio();
+        radio.setStation(0);
+        assertEquals(0,radio.getStation());
+    }
+    @Test
+    public void updateStationOverLimit(){
+        Radio radio = new Radio();
+        radio.setStation(11);
+        assertEquals(0,radio.getStation());
+    }
+    @Test
+    public void updateStationUnderLimit(){
+        Radio radio = new Radio();
+        radio.setStation(-10);
+        assertEquals(0,radio.getStation());
+    }
+    @Test
+    public void volumePlus(){
+        Radio radio = new Radio();
+        radio.plusVolume(5);
+        assertEquals(6, radio.getVolume());
+    }
+    @Test
+    public void volumeMinus(){
+        Radio radio = new Radio();
+        radio.minusVolume(10);
+        assertEquals(9, radio.getVolume());
+    }
+    @Test
+    public void volumePlusOverLimit(){
+        Radio radio = new Radio();
+        radio.plusVolume(10);
+        assertEquals(10, radio.getVolume());
+    }
+    @Test
+    public void volumeMinusOverLimit(){
+        Radio radio = new Radio();
+        radio.minusVolume(0);
+        assertEquals(0, radio.getVolume());
+    }
+    @Test
+    public void MaxVolume(){
+        Radio radio = new Radio();
+        assertEquals(10, radio.getMaxVolume());
+    }
+    @Test
+    public void MinVolume(){
+        Radio radio = new Radio();
+        assertEquals(0, radio.getMinVolume());
+    }
 }
